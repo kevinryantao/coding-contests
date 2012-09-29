b6aae3467a203d00980fa97ca1b039f25d30dacb
Kevin Ryan Tao <kevin.ryan.tao@gmail.com>
2012-09-29 00:07:29 -0500

    Doing practice interview problems

---
+++ src/crackingcodinginterview/treesandgraphs/binarytree/BinaryTreeNode.java
@@ -0,0 +1,22 @@
+package crackingcodinginterview.treesandgraphs.binarytree;
+
+public class BinaryTreeNode {
+
+    private BinaryTreeNode theLeftChild;
+    private BinaryTreeNode theRightChild;
+    private int theValue;
+
+    public BinaryTreeNode(int aValue) {
+        theValue = aValue;
+    }
+
+    public void addLeftChild(int aValue) {
+
+    }
+
+    public int getValue(){
+        return theValue;
+    }
+
+
+}
